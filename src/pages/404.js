563408b0d3007fce295d905b4c27890af6fe44f1
Helena <inger.helena@gmail.com>
2020-09-30 11:52:03 +0100

    changed IndexPage to a class

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <>
     <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a route that doesn't exist... the sadness.</p>
   </>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,25 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <>
-    <h1>Estonian Holidays</h1>
-  </>
-)
+
+class IndexPage {
+
+  constructor() {
+    super()
+
+    this.state = {
+      holidays: []
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Estonian Holidays</h1>
+      </>
+    )
+  }
+}
 
 export default IndexPage
