716ec018585b6a050e3d4e7b0b5d090e841a10d8
Helena <inger.helena@gmail.com>
2020-09-30 11:43:26 +0100

    created project

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,9 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  <>
+    <h1>Estonian Holidays</h1>
+  </>
 )
 
 export default IndexPage
